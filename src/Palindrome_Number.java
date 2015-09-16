daf1fb9bb7c777b4b9e83a8a969ba5412cc8ece2
ggjjl1 <ggjjl1@126.com>
2015-09-16 23:03:09 +0800

    判断一个数是否是回文数，并且不实用额外空间，通过。

--- src/Palindrome_Number.java
+++ src/Palindrome_Number.java
@@ -2,7 +2,24 @@
  * Created by ggjjl1 on 9/15/15.
  */
 public class Palindrome_Number {
-    public boolean isPalindrome(int x){
-        return true;
+    public static boolean isPalindrome(int x){
+        int z = 0;
+        int y = x;
+        while(x>0){
+            z = z*10 + x%10;
+            x = x/10;
+        }
+        if(z == y){
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        if(isPalindrome(12321)){
+            System.out.println("是回文数");
+        }else{
+            System.out.println("不是");
+        }
     }
 }
