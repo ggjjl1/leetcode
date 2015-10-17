d39dd5b215784800c5caec3a3a67726048d269a3
ggjjl1 <ggjjl1@126.com>
2015-10-18 00:08:44 +0800

    something AC

--- src/Word_Break.java
+++ src/Word_Break.java
@@ -5,52 +5,22 @@ import java.util.*;
  */
 public class Word_Break {
     public boolean wordBreak(String s, Set<String> wordDick){
-        int strlen = s.length();
-        /*for(String word: wordDick){
-            if(s.indexOf())
-        }*/
+        int len = 0;
+        for(String word: wordDick){
+            len += word.length();
+        }
+        if(s.length() != len){
+            return false;
+        }
         return true;
     }
 
     public static void main(String[] args) {
-        HashSet hashset = new HashSet();
-        hashset.add("hello");
-        hashset.add("world");
-        Iterator it = hashset.iterator();
-        while(it.hasNext()){
-            System.out.println(it.next());
-        }
-        String str = "Hello World!";
-        System.out.println(str.charAt(0));
-        float x = 3.1415926f;
-        float y = 1.67f;
-        System.out.println(x + y);
-
-        Gerbil geb1 = new Gerbil(11);
-        Gerbil geb2 = new Gerbil(12);
-        Gerbil geb3 = new Gerbil(13);
-        Gerbil geb4 = new Gerbil(14);
-        Gerbil geb5 = new Gerbil(15);
-        List<Gerbil> list = new ArrayList<Gerbil>();
-        list.add(geb1);
-        list.add(geb2);
-        list.add(geb3);
-        list.add(geb4);
-        list.add(geb5);
-        for(Gerbil geb: list){
-            geb.hop();
-        }
-    }
-}
-
-class Gerbil{
-    int gerbilNumber;
-
-    public Gerbil(int gerbilNumber){
-        this.gerbilNumber = gerbilNumber;
-    }
-
-    public void hop(){
-        System.out.println("the gerbil is jumping, number is " + gerbilNumber);
+        String s = "leetcode";
+        String s1 = new String("leet");
+        String s2 = new String("code");
+        Set<String> dict = new HashSet<String>();
+        dict.add(s1);
+        dict.add(s2);
     }
 }
