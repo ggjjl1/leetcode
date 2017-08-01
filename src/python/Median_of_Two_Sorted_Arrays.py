8d6bfda5c9df4b31ef9d161c5893b25b3e8bf5fa
ggjjl1 <ggjjl1@126.com>
2017-08-01 23:49:57 +0800

    Median of Two Sorted Arrays 增加一种时间复杂度为 O(m+n) 的算法

--- src/python/Median_of_Two_Sorted_Arrays.py
+++ src/python/Median_of_Two_Sorted_Arrays.py
@@ -3,6 +3,7 @@
 # from __future__ import division
 
 class Median_of_Two_Sorted_Arrays(object):
+    '''
     def findMedianSortedArrays(self, nums1, nums2):
         """
         :type nums1: List[int]
@@ -13,11 +14,48 @@ class Median_of_Two_Sorted_Arrays(object):
         nums.sort()
         half = len(nums) / 2
         return (nums[half] + nums[~half]) / 2.0
+    '''
 
+    def findMedianSortedArrays(self, nums1, nums2):
+        len1 = len(nums1)
+        len2 = len(nums2)
+        lenx = len1 + len2
+        half1 = len1 / 2
+        half2 = len2 / 2
+        i, j, k = 0, 0, 0
+        tmp, med, medx = 0, 0, 0
+        if len1 == 0:
+            return (nums2[half2] + nums2[~half2]) / 2.0
+        if len2 == 0:
+            return (nums1[half1] + nums1[~half1]) / 2.0
+        while i < lenx / 2 + 1:
+            if j < len1 and k < len2:
+                if nums1[j] < nums2[k]:
+                    tmp = nums1[j]
+                    j += 1
+                else:
+                    tmp = nums2[k]
+                    k += 1
+            else:
+                if j < len1:
+                    tmp = nums1[j]
+                    j += 1
+                if k < len2:
+                    tmp = nums2[k]
+                    k += 1
+            i += 1
+            if i == lenx / 2:
+                med = tmp
+            if i == lenx / 2 + 1:
+                medx = tmp
+        return lenx % 2 == 0 and (med + medx) / 2.0 or medx
+                 
 def main():
     median = Median_of_Two_Sorted_Arrays()
-    nums1 = [1, 3]
-    nums2 = [2]
+    # nums1 = [1, 3]
+    # nums2 = [2]
+    nums1 = [2]
+    nums2 = []
     m = median.findMedianSortedArrays(nums1, nums2)
     print(m)
 
