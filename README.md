d1ba7e8349d602ac0a8da85b068b18d4cb201c71
ggjjl1 <ggjjl1@126.com>
2020-11-04 12:10:56 +0800

    更改readme

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # leetcode算法题
 ---
 本仓库主要用来保存leetcode算法题代码，主要使用Python/Java编写。 
+* Python代码和Java代码命名风格不一样
--- java/src/leetcode/SumRootToLeafNumbers.java
+++ java/src/leetcode/SumRootToLeafNumbers.java
@@ -1,4 +1,42 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SumRootToLeafNumbers {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public int sumNumbers(TreeNode root) {
+        List<Integer> nums = new ArrayList<>();
+        int sum = 0;
+        if (root != null) {
+            treeTraversal(root, 0, nums);
+        }
+        for (int i = 0; i < nums.size(); i++) {
+            sum += nums.get(i);
+        }
+        return sum;
+    }
+
+    public void treeTraversal(TreeNode node, int num, List<Integer> nums) {
+        if (node.left == null && node.right == null) {
+            nums.add(num * 10 + node.val);
+            return;
+        }
+        if (node.left != null) {
+            treeTraversal(node.left, num * 10 + node.val, nums);
+        }
+        if (node.right != null) {
+            treeTraversal(node.right, num * 10 + node.val, nums);
+        }
+    }
 }
